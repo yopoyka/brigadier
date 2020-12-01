8d77ec5003b69b0550a1da28c8915972a51d1db4
yopoyka <yopoyka@gmail.com>
2020-12-01 15:24:07 +0300

    Make ChatComponentMessage use supplier

--- src/main/java/yopoyka/brigadier/ChatComponentMessage.java
+++ src/main/java/yopoyka/brigadier/ChatComponentMessage.java
@@ -3,19 +3,26 @@ package yopoyka.brigadier;
 import yopoyka.brigadier.com.mojang.brigadier.Message;
 import net.minecraft.util.IChatComponent;
 
-public class ChatComponentMessage implements Message {
-    private final IChatComponent component;
+import java.util.function.Supplier;
 
+public class ChatComponentMessage implements Message {
+    private final Supplier<IChatComponent> component;
+
+    @Deprecated
     public ChatComponentMessage(IChatComponent component) {
+        this(component::createCopy);
+    }
+
+    public ChatComponentMessage(Supplier<IChatComponent> component) {
         this.component = component;
     }
 
     public IChatComponent getComponent() {
-        return component;
+        return component.get();
     }
 
     @Override
     public String getString() {
-        return component.getUnformattedText();
+        return component.get().getUnformattedText();
     }
 }
